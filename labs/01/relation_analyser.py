3c97c0d5decffdd15e6a71ad13b149282634a080
BoJavs-svg <javiereric1309@gmail.com>
2023-02-23 09:25:32 -0600

    Solved

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,52 @@
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+##Javier Eric Hernández Garza A01635392
+import graphviz as gv
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+def show_graph(R):
+    dot = gv.Digraph(comment='Set', format='png')
+    for i in R:
+        dot.node(str(i[0]), str(i[0]))
+        dot.node(str(i[1]), str(i[1]))
+        dot.edge(str(i[0]), str(i[1]))
+    dot.render('Set.gv', view=True)
 
-    return Reflexive,Symmetric,Transitive
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
-    g.view()
+def is_reflexive(R):
+    for i in R:
+        if i[0]==i[1]:
+            return True
+    return False
+
+def is_symmetric(R):
+    for i in R:
+        if (i[1],i[0]) not in R:
+            return False
+    return True
+    
+def is_transitive(R):
+    dic=dict()
+    for i in R:
+        if i[0] in dic:
+            dic[i[0]].append(i[1])
+        else:
+            dic[i[0]]=[i[1]]
+    for i in R:
+        if i[1] in dic:
+            for j in dic[i[1]]:
+                if (i[0],j) not in R:
+                    return False
+    return True
+
 
 def main():
-    print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
-    print(f"\
-    1. Reflexive: {Reflexive} \
-    2. Symmetric: {Symmetric} \
-    3. Transitive: {Transitive}")
-    plot()
+    # R = {(1, 2), (1, 3), (2, 3), (3, 4)}
+    # R={ (0,0), (0,1), (0,3), (1,0), (1,1  ), (2,2), (3,0), (3,3) } 
+    R={ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
+    print(f"R = {R}")
+    print(f"R is reflexive? {is_reflexive(R)}")
+    print(f"R is symmetric? {is_symmetric(R)}")
+    print(f"R is transitive? {is_transitive(R)}")
+    show_graph(R)
+
 
 if __name__ == "__main__":
     main()
