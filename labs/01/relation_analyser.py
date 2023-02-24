69204c24b8d3a85d8a8b023703c6baeb30dbe219
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-02-23 19:49:26 -0600

    Homework finished, might be messing some more testing

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,75 @@
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+class Graph:
+    def __init__(self, str) -> None:
+        self.values = {}
+        isNode = True
+        connection = ''
+        for c in str:
+            if (c.isnumeric()):
+                if isNode: #Si es nodo
+                    if (c not in self.values): #Si no lo encuentra en el grafo
+                        self.values[c] = []
+                    isNode = False
+                    connection = c
+                else:
+                    self.values[connection].append(c)
+                    isNode = True
+                    connection = ''
 
-    return Reflexive,Symmetric,Transitive
+    
+    def print(self):
+        print(self.values)
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
-    g.view()
+    def isReflexive(self) -> bool:
+        for node in self.values:
+            if node not in self.values[node]:
+                return False
+        return True
+    
+    def isSymmetric(self) -> bool:
+        for node in self.values:
+            for relations in self.values[node]:
+                if (relations != node):
+                    if (node not in self.values[relations]):
+                        return False
+        return True
+
+    def isTransitive(self) -> bool:
+        for node in self.values:
+            for relations in self.values[node]:
+                if (relations != node):
+                    extraElement = True #En caso de que el arreglo no contenga otro elemento para tener x,y,z
+                    for z in self.values[relations]:
+                        if (z != node or z != relations):
+                            if ((node not in self.values[z]) and (z not in self.values[node])):
+                                return False
+                            else:
+                                extraElement = False
+                    if (extraElement):
+                        return False
+        return True
+    
+    def display(self) -> None:
+        # Create a graphviz graph object and display it on screen
+        d = graphviz.Digraph(format='png')
+        for node in self.values:
+            for relations in self.values[node]:
+                d.edge(node, relations)
+        d.render('test-output/round-table.gv', view=True)
+    
+    def isEquivalence(self) -> None:
+        print(f"\
+    1. Reflexive: {self.isReflexive()} \
+    2. Symmetric: {self.isSymmetric()} \
+    3. Transitive: {self.isTransitive()}")
 
 def main():
-    print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
-    print(f"\
-    1. Reflexive: {Reflexive} \
-    2. Symmetric: {Symmetric} \
-    3. Transitive: {Transitive}")
-    plot()
+    g = Graph("{ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }") 
+    #g = Graph("{(1, 1), (2, 2), (3, 3), (1, 2), (2, 3), (1, 3)}")
+    g.print()
+    g.isEquivalence()
+    g.display()
 
 if __name__ == "__main__":
     main()
