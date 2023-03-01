55ed043179316ff5ca54cb2a52ea176fd0a8d155
CarlosVP120 <carlosveryan4@gmail.com>
2023-03-01 14:13:38 -0600

    A01641147

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,8 +1,9 @@
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
-def analyze(val):
+# By Carlos Alberto Veryan Peña A01641147
+
+def analyze(set):
     """
-    Here goes your code to do the analysis
     1. Reflexive: aRa for all a in X,
     2. Symmetric: aRb implies bRa for all a,b in X
     3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
@@ -11,26 +12,50 @@ def analyze(val):
     Symmetric = False
     Transitive = False
 
+    dict = {}
+
+    for x in set:
+        start = x[0]
+        end = x[1]
+
+        if (start == end): 
+            Reflexive = True
+        else:
+            if (start not in dict):
+                dict[start] = []
+            
+            if (x[::-1] in set):
+                Symmetric = True
+            
+            dict[start].append(end)
+        
+
+    for val in dict.values():
+        if tuple(val) in set: Transitive = True
+
     return Reflexive,Symmetric,Transitive
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
+def plot(set):
+
     g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+    g.attr('node', shape='circle')
+    g.attr(rankdir='LR')
+    for x in set:
+        g.edge(str(x[0]), str(x[1]))
+
     g.view()
 
 def main():
     print("Hello World analyzing input!")
-    val = input("Enter your set: ")
+    val = input("Enter a set: ")
+    val = set(eval(val))
     print(val)
     Reflexive,Symmetric,Transitive = analyze(val)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(val)
 
 if __name__ == "__main__":
     main()
