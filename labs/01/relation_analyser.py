13cc20081f23f8ba89a4df4f14a68dc8743e55d5
Juan Zambrano <jpablozambrano@hotmail.com>
2023-03-11 16:50:13 -0600

    Lab_01_A01636420

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,55 @@
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+import graphviz  # https://graphviz.readthedocs.io/en/stable/index.html
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+def analyze(val, alphabet):
+  Reflexive = True
+  Symmetric = True
+  Transitive = True
+  #Reflexive
+  for value in alphabet:
+    if (value, value) not in val: 
+      Reflexive = False
+      break
+  #Symmetric
+  for tupla in val:
+    if (tupla[0], tupla[1]) not in val or tupla[1] not in alphabet or tupla[0] not in alphabet:
+      Symmetric = False
+      break
+  #Transitive
+  for tupla1 in val:
+    for tupla2 in val:
+      if (tupla1[0], tupla2[1]) not in val:
+        Transitive = False
+        break
+  return Reflexive, Symmetric, Transitive
 
-    return Reflexive,Symmetric,Transitive
-
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
-    g.view()
+def plot(val):
+  g = graphviz.Digraph('G', filename='graph.log')
+  for tuple in val:
+    g.edge(str(tuple[0]),str(tuple[1]))
+  g.view()
 
 def main():
-    print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
-    print(f"\
+  print("Hello World analyzing input!")
+  #val = input("Enter your set: ")
+  #Set which is Symmetric, Reflexive and Transitive for testing
+  #conjunto_tuplasSRT = "{(1,1), (2,2), (3,3), (1,2), (2,1), (1,3), (3,1), (2,3), (3,2)}"
+  #alfabetoSRT = { 1, 2, 3}
+  #So we dont take half an hour writing 
+  val = "{ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }"
+  print(val)
+  #Alphabet is defined
+  alphabet = {0, 1, 2, 3}
+  #Spaces and periods are removed
+  val = val.replace(' ', '').replace('.', '')
+  #The string becomes a set of tuples
+  set_tuple = set(eval(val))
+  #function called
+  Reflexive, Symmetric, Transitive = analyze(set_tuple, alphabet)
+  print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+  plot(set_tuple)
 
 if __name__ == "__main__":
-    main()
+  main()
