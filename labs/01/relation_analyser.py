ca57c6889f76188d12b544458a193733f85b605d
Juan Salazar <jjsc2003@live.com>
2023-03-11 19:49:39 -0600

    Lab 01 Completed Successfully

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,92 @@
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+import graphviz  # https://graphviz.readthedocs.io/en/stable/index.html d
+"""
+Name: Juan José Salazar Cortés
+ID: A01642126
+Date: Saturday March 11 2023
+"""
+# Input:
+# # A = { 0,1,2,3 }
+# # R = { (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
+# Output:
+# 1. Reflexive: True     2. Symmetric: True     3. Transitive: False
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
+
+# Input:
+# A = {1,2,3,4}
+# R = {(1,1), (2,2), (2,4), (3,1), (3,2), (3,4), (4,2), (4,4)}
+# Output:
+# 1. Reflexive: False     2. Symmetric: False     3. Transitive: True
+
+
+def analyze(set, relations):
+
+    #Initial States of the Answers
+    Reflexive = True
+    Symmetric = True
     Transitive = False
 
-    return Reflexive,Symmetric,Transitive
+    # Cleaning the inputs to make them usable
+    symbols = '}{)(,'
+    relationsCleaned = relations.translate(str.maketrans('', '', symbols))
+    relations = relationsCleaned.split()
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+    setCleaned = set.translate(str.maketrans('', '', symbols))
+    set = setCleaned
+
+    # Checks if is Reflexive
+    for num in set:
+        reflexiveRelation = num + num
+        if (reflexiveRelation not in relations):
+            Reflexive = False
+            break
+    
+    # Checks if is Symmetric
+    for num in relations:
+        if (num[::-1] not in relations):
+            Symmetric = False
+            break
+
+    # Checks if is Transitive
+    for i in range(len(relations)):
+        x = relations[i][0]
+        y = relations[i][1]
+        if (x != y):
+            for j in range(len(relations)):
+                if (y == relations[j][0] and x != relations[j][1]):
+                    k = relations[j][1]
+                    for z in range(len(relations)):
+                        if (x == relations[z][0] and relations[z][1] == k and y != k):
+                            Transitive = True
+                            break
+
+    return Reflexive, Symmetric, Transitive
+
+# Creates the graph with the relations given
+def plot(relations):
+    
+    # Cleaning the inputs to make them usable
+    symbols = '}{)(,'
+    relationsCleaned = relations.translate(str.maketrans('', '', symbols))
+    relations = relationsCleaned.split()
+    g = graphviz.Digraph('G', filename='graph.log')
+    g.edges(relations)
     g.view()
 
+
 def main():
-    print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
+
+    set = input("Enter your set: ")
+    relations = input("Enter your relations: ")
+
+    Reflexive, Symmetric, Transitive = analyze(set, relations)
+
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+
+    plot(relations)
+
 
 if __name__ == "__main__":
     main()
