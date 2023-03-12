5c82b0b5abfadacde31c8bd61773c43346a786d8
NolbertoCastro <nolber0703@gmail.com>
2023-03-11 20:25:33 -0600

    Great solutions require great Stress

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,142 @@
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
+#Nolberto Castro Sánchez
+#A01641501
+#11 de Marzo del 2023
+
+def translate(val):
+    array = []
+    empty = []
+    openbrackets = []
+
+    for n in val:
+        if (n == "("):
+            openbrackets.append("(")
+        elif (n == ")"):
+            openbrackets.pop()
+            array.append(empty)
+            empty = []
+
+        if (len(openbrackets) > 0 and n != "," and n != "("):
+            empty.append(n)
+    return array
+
 def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+    #Reflexive Analisis
+    nodes = []
+    reflex = []
+
+    for n in val:
+        if (n[0] not in nodes):
+            nodes.append(n[0])
+            reflex.append(n[0])
+        
+        if (n[0] in nodes):
+            if (n[0] == n[1]):
+                value = reflex.index(n[0])
+                reflex.pop(value)
+
+    #Symmetric Analisis
+
+    sym = []
+    for n in val:
+        if (n[0] != n[1]):
+            pb1 = n[0] + n[1]
+            pb2 = n[1] + n[0]
+            if (pb1 in sym and pb2 in sym):
+                value1 = sym.index(pb1)
+                value2 = sym.index(pb2)
+                sym.pop(value1)
+                sym.pop(value2)
+            else:
+                sym.append(pb1)
+                sym.append(pb2)
+
+    #Transitive Analisis
+
+    nodesTotal = []
+    posibilities = []
+
+    for n in val:
+        if (n[0] not in nodesTotal):
+            nodesTotal.append(n[0])
+        if (n[1] not in nodesTotal):
+            nodesTotal.append(n[1])
+
+    for n in val:
+        for i in val:
+            if (n != i):
+                pos1 = n+i
+                pos2 = i+n
+                if (pos1 not in posibilities and pos2 not in posibilities):
+                    posibilities.append(pos1)
+                    posibilities.append(pos2)
+    
+    for n in val:
+        if (n[0] != n[1]):
+            pb1 = n[0] + n[1]
+            pb2 = n[1] + n[0]
+            if (pb1 in posibilities and pb2 in posibilities):
+                value1 = posibilities.index(pb1)
+                value2 = posibilities.index(pb2)
+                posibilities.pop(value1)
+                posibilities.pop(value2)
+
+    
+    if (len(reflex) == 0):
+        Reflexive = True
+    else:
+        Reflexive = False
+
+    if (len(sym) == 0):
+        Symmetric = True
+    else:
+        Symmetric = False
+
+    if (len(posibilities) == 0):
+        Transitive = True
+    else:
+        Transitive = False
 
     return Reflexive,Symmetric,Transitive
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
+def plot(graph):
     g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+    for n in graph:
+        start = n[0]
+        end = n[1]
+        g.edge(start, end)
     g.view()
+
+def graphScript(graph):
+    f = open("graph.log", "x")
+    f.write("""digraph example {\n
+	rankdir=LR;
+	node [shape = circle];\n""")
+    for n in graph:
+        start = n[0]
+        end = n[1]
+        f.write("\t" + start +  " -> " +  end + ";\n")
+    f.write("\n}")
+    f.close()
 
 def main():
     print("Hello World analyzing input!")
-    val = input("Enter your set: ")
+    val = translate(input("Enter your set: "))
     print(val)
     Reflexive,Symmetric,Transitive = analyze(val)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+
+    if (Reflexive and Symmetric and Transitive):
+        print("The set does have equivalence relation")
+    else:
+        print("The set does not have equivalence relation")
+        
+    plot(val)
+    graphScript(val)
 
 if __name__ == "__main__":
     main()
