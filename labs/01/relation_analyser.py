aa59549982e04b8b978821019a0d072d21627289
Israel Vidal <73002064+IsraelVidalParedes@users.noreply.github.com>
2023-03-11 22:46:40 -0600

    Lab 01 Initial Commit Israel Vidal

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,64 @@
+#Analizador de relaciones
+#Israel Vidal Paredes A01750543
+
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+
+def isReflexive(val):
+    for x in val:
+        if (x[0] == x[1]):
+            return True
+    return False
+
+def isSymmetric(val):
+    for x in val:
+        if (x[1],x[0]) not in val:
+            return False
+    return True
+
+def isTransitive(val):
+    for (a, b) in val:
+            for (c, d) in val:
+                if b == c and (d, a) not in val:
+                    return False
+    return True
 
 def analyze(val):
     """
-    Here goes your code to do the analysis
     1. Reflexive: aRa for all a in X,
     2. Symmetric: aRb implies bRa for all a,b in X
     3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
     """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+    
+    R = set(eval(val))
+
+    Reflexive = isReflexive(R)
+    Symmetric = isSymmetric(R)
+    Transitive = isTransitive(R)
 
     return Reflexive,Symmetric,Transitive
 
-def plot():
+def plot(val):
     """
     Here goes your code to do the plot of the set
     """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+    R = set(eval(val))
+    g = graphviz.Digraph('G', filename='graph.gv')
+
+    for x in R:
+        g.edge(str(x[0]), str(x[1]))
     g.view()
 
 def main():
     print("Hello World analyzing input!")
-    val = input("Enter your set: ")
+    # val = input("Enter your set: ")
+    val = "{ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }"
     print(val)
     Reflexive,Symmetric,Transitive = analyze(val)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(val)
 
 if __name__ == "__main__":
     main()
