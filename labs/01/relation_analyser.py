1c53b17b76032d5aaed3bae38a4a842e28830a36
Iker8av <iker8avv@gmail.com>
2023-03-11 21:02:11 -0600

    [Lab 01] - Finished Task

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,6 +1,6 @@
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+import graphviz  # https://graphviz.readthedocs.io/en/stable/index.html
 
-def analyze(val):
+def analyze(R):
     """
     Here goes your code to do the analysis
     1. Reflexive: aRa for all a in X,
@@ -11,26 +11,66 @@ def analyze(val):
     Symmetric = False
     Transitive = False
 
-    return Reflexive,Symmetric,Transitive
+    tempState = ""
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
+    for states in R:
+        # Analyze if it is Reflexive
+        if (states[0] == states[1]):
+            Reflexive = True
+
+        # Analyze if it is Symmetric
+        if (states[0] != states[1] and not Symmetric):
+            Symmetric = analizeSymmtric(R, states)
+
+        # Analyze if it is Transitive
+        if (states[0] != states[1] and not Transitive):
+            Transitive = analizeTransitive(R, states)
+
+    return Reflexive, Symmetric, Transitive
+
+
+def analizeSymmtric(R, actState):
+    for states in R:
+        if (actState[1] == states[0] and actState[0] == states[1]):
+            return True
+    return False
+
+
+def analizeTransitive(R, actState):
+    tmpState = []
+    firstState = actState[0]
+
+    for states in R:
+        if (actState[1] == states[0] and states[0] != states[1] and states[1] != actState[0]):
+            tmpState = [states[0], states[1]]
+
+            for states2 in R:
+                if (tmpState and firstState == states2[0] and states2[1] == tmpState[1]):
+
+                    return True
+    return False
+
+
+def plot(R):
     g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+
+    for states in R:
+        g.edge(str(states[0]), str(states[1]))
+
     g.view()
+
 
 def main():
     print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
+    R = { (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
+    print(R)
+    Reflexive, Symmetric, Transitive = analyze(R)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(R)
+
 
 if __name__ == "__main__":
     main()
