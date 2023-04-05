8bafe62745359bc6c4bbbf0841e6c0af4fc5dca0
flioner <lionerrochae@gmail.com>
2023-04-04 23:16:11 -0600

    explicación práctica

--- labs/02/lab2.py
+++ labs/02/lab2.py
@@ -54,3 +54,36 @@ for i in nivel_nfa:
 
 # Falta la logica que conecta los niveles con las flechas de los nodos
 # graphviz sigue sin funcionar, entonces no sé que tan inecesariamente complicado lo estoy haciendo
+
+# falta tomar mi output, y recorrer nivel por nivel
+
+
+# si encuentra una U o más en cualquier nivel, agrega un nodo vació del cual se desglosan las dos opciones de la union
+
+# 0->1 [label= SS(E)] 'esto es para el "ba" '
+# 0->2 [label= SS(E)] 'esto es para el Nivel 2.1'
+# 0->3 [label= SS(E)] 'esto es para el Nivel 2.2'
+
+# si encuentra un * en cualquier nivel, agrega una flecha del ultimo elemennto al primero, entre en parentesis de ser necesario
+
+# de ahí en más, conecta los nodos con flechas, llevando un contador global
+
+# 0->1 [label= SS(E)] 'esto es para el "ba" '
+
+# 1->4 [label= S(b)] 'esto es para el "b" del "ba" '
+# 4->5 [label= S(#end a)] 'esto es para el "a" del "ba" '
+
+# 0->2 [label= SS(E)] 'esto es para el Nivel 2.1 (ab U Nivel 3.1)'
+# 2->6 [label= S(E)] 'esto es para el ab de la union' del Nivel 2.1'
+# 6->7 [label= S(a)] 'esto es para el "a" del "ab" '
+# 7->8 [label= S(b)] 'esto es para el "b" del "ab" '
+
+# 2->9 [label= S(E)] 'esto es para el Nivel3.1 en la union' del Nivel 2.1'
+# hacemos el degloce del Nivel 3.1, esto es recursivo
+
+# pero ya llevo bastante tiempo atorado, entonces lo voy a dejar así por el momento
+# voy a volver a intentarlo desde mi laptop linux, pero sí creo que lo estoy haciendo
+# incesariamente complicado, y que hay una forma más sencilla de hacerlo
+
+# sé que esta lógica funciona, pero voy a tener que refactorizar
+
