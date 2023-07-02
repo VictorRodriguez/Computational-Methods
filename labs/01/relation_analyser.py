97d30c404b6f02409015bdb22ecd4d3c777bb0ae
JonathanChavezTamales <jonathanchatab@hotmail.com>
2023-07-01 22:50:04 -0600

    lab 1

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,4 +1,6 @@
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+import ast
+import graphviz  # https://graphviz.readthedocs.io/en/stable/index.html
+
 
 def analyze(val):
     """
@@ -11,26 +13,92 @@ def analyze(val):
     Symmetric = False
     Transitive = False
 
-    return Reflexive,Symmetric,Transitive
+    X = {i[0] for i in val}
+    X.update({i[1] for i in val})
 
-def plot():
+    # Check if the set is Reflexive
+    for i in X:
+        if (i, i) in val:
+            Reflexive = True
+        else:
+            Reflexive = False
+            break
+
+    # Check if the set is Symmetric
+    for i in val:
+        if (i[1], i[0]) in val:
+            Symmetric = True
+        else:
+            Symmetric = False
+            break
+
+    # Check if the set is Transitive
+    for i in val:
+        for j in val:
+            if i[1] == j[0]:
+                if (i[0], j[1]) in val:
+                    Transitive = True
+                else:
+                    Transitive = False
+                    break
+
+    return Reflexive, Symmetric, Transitive
+
+
+def plot(val):
     """
-    Here goes your code to do the plot of the set
+    Here goes your code to generate graph.log and plot the graph
     """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+
+    # Create the graph
+    g = graphviz.Digraph('G', filename='graph.log')
+    g.attr(rankdir='LR', size='8,5')
+
+    for i in val:
+        g.edge(str(i[0]), str(i[1]))
+
+    # Render the graph
     g.view()
+
+
+def get_set(val):
+    """
+    Here goes your code to get the set from the input
+    Example: { (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
+    """
+    result = None
+
+    try:
+        # Use literal_eval to convert the string into a set
+        result = ast.literal_eval(val)
+
+        # Check if result is a set
+        if not isinstance(result, set):
+            raise ValueError("Input string does not represent a set")
+
+    except ValueError as e:
+        print(f"Error: Invalid input. {e}")
+        raise e
+    except Exception as e:
+        print("Error: Something went wrong.")
+        raise e
+
+    return result
+
 
 def main():
     print("Hello World analyzing input!")
+    # Example: 	{ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
     val = input("Enter your set: ")
+    val = get_set(val)
     print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
+    Reflexive, Symmetric, Transitive = analyze(val)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(val)
+
 
 if __name__ == "__main__":
     main()
