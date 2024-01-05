bdfc2d518ae8a00ce8992d403ba9d115adf0d1a4
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2024-01-05 11:31:40 -0600

    feat: La funcion plot esta en funcionamiento

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,3 +1,7 @@
+"""
+ @Miguel Angel Cabrera Victoria <A01782982@tec.mx>
+ Input : {(0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3)} 
+"""
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
 def analyze(val):
@@ -13,24 +17,32 @@ def analyze(val):
 
     return Reflexive,Symmetric,Transitive
 
-def plot():
+def plot(val):
     """
     Here goes your code to do the plot of the set
     """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+    g = graphviz.Digraph('G', filename='graph.gv')
+
+    pairs = [pair.replace("{", "").replace("}", "").replace("(", "").replace(")", "").strip(',') for pair in val.split(" ")]
+    #print(pairs)
+    pair_lists = [[int(element) for element in pair.split(',')] for pair in pairs]
+    print(pair_lists)
+    #print(pairs)
+
+    for pair in pair_lists:
+        #print(pair[0], " ", pair[1])
+        g.edge(str(pair[0]), str(pair[1]))
     g.view()
 
 def main():
-    print("Hello World analyzing input!")
+    print("Miguel Angel Cabrera Victoria - A01782982")
     val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
-    print(f"\
-    1. Reflexive: {Reflexive} \
-    2. Symmetric: {Symmetric} \
-    3. Transitive: {Transitive}")
-    plot()
+    # Reflexive,Symmetric,Transitive = analyze(val)
+    # print(f"\
+    # 1. Reflexive: {Reflexive} \
+    # 2. Symmetric: {Symmetric} \
+    # 3. Transitive: {Transitive}")
+    plot(val)
 
 if __name__ == "__main__":
     main()
