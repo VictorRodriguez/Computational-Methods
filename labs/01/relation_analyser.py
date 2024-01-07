1cae065ef2dba2cf7ea7fae13e0a9eb835cf2e8f
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2024-01-07 12:20:47 -0600

    feat: relacion reflexiva

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,48 +1,65 @@
 """
  @Miguel Angel Cabrera Victoria <A01782982@tec.mx>
- Input : {(0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3)} 
+ Input : {(0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3)}
+        {(0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3), (4,1), (4,4), (5,5)}
 """
-import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
+import graphviz 
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
-    Reflexive = False
-    Symmetric = False
-    Transitive = False
+def reflexive(pair_lists):
+    result = []
+    max = 0
+    for element in pair_lists:
+        if element[0] == element[1]:
+            max = element[0]
+            result.append([element[0],element[1]])
+        
+    #print("Result ", len(result))
+    if len(result) == max + 1:
+        return True
+    return False
 
-    return Reflexive,Symmetric,Transitive
+def transitive(pair_lists):
+    pass
 
-def plot(val):
+#------------------------------------------------------------
+
+def plot(pair_lists):
     """
     Here goes your code to do the plot of the set
     """
     g = graphviz.Digraph('G', filename='graph.gv')
 
-    pairs = [pair.replace("{", "").replace("}", "").replace("(", "").replace(")", "").strip(',') for pair in val.split(" ")]
-    #print(pairs)
-    pair_lists = [[int(element) for element in pair.split(',')] for pair in pairs]
-    print(pair_lists)
-    #print(pairs)
-
     for pair in pair_lists:
-        #print(pair[0], " ", pair[1])
+        # print(pair[0], " ", pair[1])
         g.edge(str(pair[0]), str(pair[1]))
-    g.view()
+    #g.view()
+
+
+    
+    Reflexive = reflexive(pair_lists)
+    #print(Reflexive)
+    # print(f"\
+    # 1. Reflexive: {Reflexive} \
+    # 2. Symmetric:  \
+    # 3. Transitive:")
+    plot(pair_lists)
 
 def main():
     print("Miguel Angel Cabrera Victoria - A01782982")
     val = input("Enter your set: ")
-    # Reflexive,Symmetric,Transitive = analyze(val)
-    # print(f"\
-    # 1. Reflexive: {Reflexive} \
-    # 2. Symmetric: {Symmetric} \
-    # 3. Transitive: {Transitive}")
-    plot(val)
+
+    pairs = [pair.replace("{", "").replace("}", "").replace("(", "").replace(")", "").strip(',') for pair in val.split(" ")]
+    #print(pairs)
+    pair_lists = [[int(element) for element in pair.split(',')] for pair in pairs]
+    #print(pair_lists)
+
+    # Output 
+    
+    Reflexive = reflexive(pair_lists)
+
+    print(f"\nReflexive: {Reflexive}\n")
+    
+    
 
 if __name__ == "__main__":
     main()
